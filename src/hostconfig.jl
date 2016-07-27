d68152b81fc268ff69eb3686b732fb9ac0696b00
Felipe Noronha <felipenoris@gmail.com>
2016-07-26 21:57:39 -0300

    add more packages to test set

--- src/hostconfig.jl
+++ src/hostconfig.jl
@@ -11,18 +11,21 @@ push!(HOST.packages, PkgRef("BusinessDays"))
 push!(HOST.packages, PkgRef("Calculus"))
 push!(HOST.packages, PkgRef("Clp"))
 push!(HOST.packages, PkgRef("Compat"))
+push!(HOST.packages, PkgRef("DataStreams"))
+push!(HOST.packages, PkgRef("DataFrames"))
 push!(HOST.packages, PkgRef("FixedSizeArrays"))
 push!(HOST.packages, PkgRef("HttpParser"))
 push!(HOST.packages, PkgRef("HttpServer"))
 push!(HOST.packages, PkgRef("InterestRates"))
 push!(HOST.packages, PkgRef("JSON"))
 push!(HOST.packages, PkgRef("Logging"))
+push!(HOST.packages, PkgRef("MappedArrays"))
+push!(HOST.packages, PkgRef("NaturalSort"))
 push!(HOST.packages, PkgRef("Optim"))
 push!(HOST.packages, PkgRef("PyCall"))
 push!(HOST.packages, PkgRef("StatsBase"))
 
-# known to fail: Cbc, Coverage, DataStructures, ForwardDiff, Gadfly, Images, JuMP, LightXML, NullableArrays, RCall
-
+# known to fail: Cbc, Coverage, DataStructures, ForwardDiff, Gadfly, Images, JuMP, LightXML, NullableArrays, RCall, FilePaths
 
 # Set last known Commit which all tests pass
 HOST.tail_sha = "0030eec2f332f353e6890ca289ac2aca55532dde"
