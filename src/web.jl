998c7b02caee07707fca0c2689fa80378568e362
Felipe Noronha <felipenoris@gmail.com>
2016-08-05 23:38:03 -0300

    html rendering

--- src/web.jl
+++ src/web.jl
@@ -6,10 +6,54 @@ function start_webapp(ip=HOST.webapp_ip, port=HOST.webapp_port)
 		if !ismatch(r"^/julianne",req.resource)
 			Response(404)
 		else
-			Response(report_str())
+			Response(report_html())
 		end
 	end
 
 	server = Server(http)
 	@async run(server, host=ip, port=port)
+end
+
+function report_html() # :: String
+    tail = gettail()
+    r = UTF8String("")
+    r = r * """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset=\"UTF-8\">
+        <meta name=\"description\" content=\"Julianne.jl status\">
+    </head>
+    <body>
+    <ul>
+        <li> Current tail is: <b> $(tail.sha)-$(tail.subject) </b></li>
+        <li> # of available workers: <b> $(length(HOST.workers)) </b></li>
+    </ul>
+    
+    <p>
+    """
+    for c in HOST.commits
+        r = r * "<br> $(sha_abbrev(c))-$(c.subject): $(getstatus(c))\n"
+        
+        if isdone(c)
+            failures, stvec = gettestedpkg(c, [:FAILED, :UNKNOWN])
+            if !isempty(failures)
+                for i in 1:length(failures)
+                    pkg = failures[i]
+                    st = stvec[i]
+                    r = r * "<br>⎿ $(pkg.name) $(st)\n"
+                end
+            end
+        end
+
+        if istail(c)
+            break
+        end
+    end
+    r = r * """
+    </p>
+    </body>
+    </html>
+    """
+    r
 end
